0a6b488670aa661528f21fb9fe4e2f83828a53ca
Wandeon <wandeon@gmail.com>
2025-08-16 13:30:02 +0200

    feat: configure sentry

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,1 +1,7 @@
-// TODO: implement
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  tracesSampleRate: 0.2
+});
+
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,16 @@
-// TODO: implement
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 0.2,
+  environment: process.env.ENVIRONMENT,
+  release: process.env.RELEASE
+});
+
+export const handleError = ({ error }) => {
+  Sentry.captureException(error);
+  return {
+    message: 'Internal error'
+  };
+};
+
