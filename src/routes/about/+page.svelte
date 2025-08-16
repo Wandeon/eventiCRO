17bbda54ed42814ee8c219b2fe3396b41bc46122
Wandeon <wandeon@gmail.com>
2025-08-16 13:20:47 +0200

    chore: scaffold project structure

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+// TODO: implement about page
+</script>
+
+<main>
+  <h1>About</h1>
+</main>
---
+++ src/routes/event/[id]/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+// TODO: implement event detail page
+</script>
+
+<main>
+  <h1>Event Detail</h1>
+</main>
